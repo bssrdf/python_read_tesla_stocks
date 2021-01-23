7e3967b59c2e8dd27c96b83a3d898ce7dd317e19
bssrdf <merlintiger@hotmail.com>
2021-01-22 21:48:29 -0500

    changed to reading json file to get tickers

--- stock_hist.py
+++ stock_hist.py
@@ -7,17 +7,21 @@ from bs4 import BeautifulSoup
 import json
 from pprint import pprint
 
-tickers = ['BABA', 'AAPL', 'TSLA']
+#tickers = ['BABA', 'AAPL', 'TSLA', 'FB', 'GOOG', 'TWTR', 'GM','CIDM']
 stockname = []
-for ticker in tickers:
-    stock_company = f"https://finance.yahoo.com/quote/{ticker.lower()}"
-    soup = BeautifulSoup(requests.get(stock_company).text, "html.parser")
-    name = soup.h1.text.split('(')[0].strip()
-    ticker_data_url = f"https://query1.finance.yahoo.com/v8/finance/chart/{ticker}?region=US&lang=en-US&includePrePost=false&interval=2m&range=1d&corsDomain=finance.yahoo.com&.tsrc=finance"
-    ticker_data = json.loads(requests.get(ticker_data_url).text)
-    price = ticker_data['chart']['result'][0]['meta']['previousClose']
-    if name:
-        stockname.append( [ticker, name, price] )
+import json
+with open("stocks.json", "r") as read_file:
+    tickers = json.load(read_file)
+    #print(stocks)
+    for ticker in tickers['stocks']:
+        stock_company = f"https://finance.yahoo.com/quote/{ticker.lower()}"
+        soup = BeautifulSoup(requests.get(stock_company).text, "html.parser")
+        name = soup.h1.text.split('(')[0].strip()
+        ticker_data_url = f"https://query1.finance.yahoo.com/v8/finance/chart/{ticker}?region=US&lang=en-US&includePrePost=false&interval=2m&range=1d&corsDomain=finance.yahoo.com&.tsrc=finance"
+        ticker_data = json.loads(requests.get(ticker_data_url).text)
+        price = ticker_data['chart']['result'][0]['meta']['previousClose']
+        if name:
+            stockname.append( [ticker, name, price] )
 
 pprint(stockname, width=60)
 
